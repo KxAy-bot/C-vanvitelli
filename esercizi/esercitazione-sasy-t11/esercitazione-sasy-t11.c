a110c2a1df67c9743f8852470e2d0d0f0d1fc87a
cybernetkay <kxaythegod@gmail.com>
2024-11-23 17:14:50 +0100

    esercitazione sasy t11 penso giusta?

--- esercizi/esercitazione-sasy-t11/esercitazione-sasy-t11.c
+++ esercizi/esercitazione-sasy-t11/esercitazione-sasy-t11.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define nMax 100
 
@@ -10,6 +11,88 @@ typedef struct{
   char dels[nMax]; // contiene i soli delimitatori trovati nella stringa, presi una sola volta
 } MyStruct;
 
+void fillStruct(MyStruct** vec, int* size, const char* filename);
+void findDel(char* line, char* del, char* dels);
+void findIndex(char* line, char* token, int tokens[nMax][2], int n);
+void printStruct(MyStruct* vec, int size);
+
 int main(){
+  MyStruct* vec = NULL;
+  int size;
+
+  fillStruct(&vec, &size, "input.txt");
+  printStruct(vec, size);
+
+  free(vec);
   return 0; 
+}
+
+void fillStruct(MyStruct** vec, int* size, const char* filename){
+  FILE *input = fopen(filename, "r");
+  char line[nMax];
+  char del[nMax];
+  char dels[nMax];
+  int count = 0, n;
+
+  if(!input){
+    printf("Errore nell'apertura del file %s\n", filename);
+    exit(-1);
+  }
+
+  strcpy(del, fgets(line, sizeof(line), input));
+
+  if(del[strlen(del)-1] == '\n')
+    del[strlen(del)-1] = '\0';
+
+  while(fgets(line, sizeof(line), input)){
+    count++;
+    *vec = realloc(*vec, count * sizeof(MyStruct));
+    
+    strcpy((*vec)[count-1].line, line);
+
+    char lineCopy[nMax];
+    strcpy(lineCopy, line);
+
+    char* token = strtok(lineCopy, del);
+    findDel(line, del, dels);
+
+    while(token != NULL){
+      n++;
+      findIndex(line, token, (*vec)[count-1].tokens, n - 1);
+      token = strtok(NULL, del);
+    }
+
+    (*vec)[count-1].n = n;
+    strcpy((*vec)[count-1].dels, dels);
+  }
+
+  *size = count;
+  fclose(input);
+}
+
+void findDel(char* line, char* del, char* dels) {
+  int count = 0;
+
+  for (int i = 0; i < strlen(line); i++) {
+    if (strchr(del, line[i]) != NULL) {
+      if (strchr(dels, line[i]) == NULL) {
+        dels[count++] = line[i];
+      }
+    }
+  }
+
+  dels[count] = '\0';
+}
+
+void findIndex(char* line, char* token, int tokens[nMax][2], int n){
+  char* p = strchr(line, token[0]);
+  tokens[n][0] = (int)(p - line);
+  p = strchr(line, token[strlen(token)-1]);
+  tokens[n][1] = (int)(p - line);
+}
+
+void printStruct(MyStruct* vec, int size){
+  for(int i = 0; i < size; i++){
+    printf("{%s, %d, [%d %d], [%s]", vec[i].line, vec[i].n, vec[i].tokens[0][0], vec[i].tokens[0][1], vec[i].dels);
+  }
 }
